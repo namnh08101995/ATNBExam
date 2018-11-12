eaa4a21eeedc4297b46585ae240fccf8c905b1d2
namnh08101995 <44769993+namnh08101995@users.noreply.github.com>
2018-11-12 15:11:50 +0700

    aaaa

--- ATNB/ATNB.Web/Controllers/HomeController.cs
+++ ATNB/ATNB.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using ATNB.Service.Abstractions;
+using LumenWorks.Framework.IO.Csv;
+using System.Data;
 using System.IO;
 using System.Web;
 using System.Web.Mvc;
@@ -9,11 +11,13 @@ namespace ATNB.Web.Controllers
     {
         IAirPlaneService _AirPlaneService;
         IAirPortService _AirPortService;
+        IHelicopterService _HelicopterService;
 
-        public HomeController(IAirPlaneService AirPlaneService, IAirPortService AirPortService)
+        public HomeController(IAirPlaneService AirPlaneService, IAirPortService AirPortService, IHelicopterService HelicopterService)
         {
             _AirPlaneService = AirPlaneService;
             _AirPortService = AirPortService;
+            _HelicopterService = HelicopterService;
         }
 
         // GET: Home
@@ -49,7 +53,6 @@ namespace ATNB.Web.Controllers
                 //Read the contents of CSV file.
                 string csvData = System.IO.File.ReadAllText(filePath);
 
-                //Execute a loop over the rows.
                 //Call method ExecuteFileCSV(csvData);
                 ExecuteFileCSV(csvData);
             }
@@ -58,37 +61,120 @@ namespace ATNB.Web.Controllers
                 ModelState.AddModelError("File", "This file format is not supported");
                 return View();
             }
+            ViewBag.Message = "Load file successfull!";
             return View();
         }
 
         public void ExecuteFileCSV(string csvData)
         {
-
-
+            //Execute a loop over the rows.
             foreach (string row in csvData.Split('\n'))
             {
-                SaveToDatabase(row);
-
+                if (!string.IsNullOrEmpty(row))
+                {
+                    SaveToDatabase(row);
+                }
             }
         }
         public void SaveToDatabase(string row)
         {
-            string id = "";
-            if (!string.IsNullOrEmpty(row))
+            string id = (row.Split(',')[0]).Trim();
+            string subId = id.Substring(0, 2);
+            switch (subId)
             {
-                id = row.Split(',')[0];
+                case "AP":
+                    _AirPortService.Create(new Model.AirPort
+                    {
+                        Id = id,
+                        Name = row.Split(',')[1],
+                        RunwaySize = int.Parse(row.Split(',')[2]),
+                        MaxFWParkingPlace = int.Parse(row.Split(',')[3]),
+                        MaxRWParkingPlace = int.Parse(row.Split(',')[5])
+                    });
 
-                _AirPlaneService.Create(new Model.AirPlane
-                {
-                    Id = row.Split(',')[0],
-                    Model = row.Split(',')[1],
-                    AirPlaneType = row.Split(',')[2],
-                    CruiseSpeed = double.Parse(row.Split(',')[3]),
-                    EmptyWeight = double.Parse(row.Split(',')[4]),
-                    MaxTakeoffWeight = double.Parse(row.Split(',')[5]),
-                    MinNeededRunwaySize = double.Parse(row.Split(',')[6])
-                });
+                    string[] listFWId = (row.Split(',')[4]).TrimEnd().Split(' ');
+                    foreach (string FWId in listFWId)
+                    {
+                        Model.AirPlane airplane = _AirPlaneService.GetById(FWId);
+                        airplane.AirPortId = id;
+                        _AirPlaneService.Update(airplane);
+                    }
+
+                    string[] listRWId = (row.Split(',')[6]).TrimEnd().Split(' ');
+                    foreach (string RWId in listRWId)
+                    {
+                        Model.Helicopter helicopter = _HelicopterService.GetById(RWId);
+                        helicopter.AirPortId = row.Split(',')[0];
+                        _HelicopterService.Update(helicopter);
+                    }
+                    break;
+                case "FW":
+                    _AirPlaneService.Create(new Model.AirPlane
+                    {
+                        Id = id,
+                        Model = row.Split(',')[1],
+                        AirPlaneType = row.Split(',')[2],
+                        CruiseSpeed = double.Parse(row.Split(',')[3]),
+                        EmptyWeight = double.Parse(row.Split(',')[4]),
+                        MaxTakeoffWeight = double.Parse(row.Split(',')[5]),
+                        MinNeededRunwaySize = double.Parse(row.Split(',')[6])
+                    });
+                    break;
+                case "RW":
+                    _HelicopterService.Create(new Model.Helicopter
+                    {
+                        Id = id,
+                        Model = row.Split(',')[1],
+                        CruiseSpeed = double.Parse(row.Split(',')[2]),
+                        EmptyWeight = double.Parse(row.Split(',')[3]),
+                        MaxTakeoffWeight = double.Parse(row.Split(',')[4]),
+                        Range = double.Parse(row.Split(',')[5])
+                    });
+                    break;
+                default:
+                    ViewBag.Message = "Load file false";
+                    break;
             }
+        }
+
+
+        public ActionResult Upload()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Upload(HttpPostedFileBase upload)
+        {
+            if (ModelState.IsValid)
+            {
+
+                if (upload != null && upload.ContentLength > 0)
+                {
+
+                    if (upload.FileName.EndsWith(".csv"))
+                    {
+                        Stream stream = upload.InputStream;
+                        DataTable csvTable = new DataTable();
+                        using (CsvReader csvReader =
+                            new CsvReader(new StreamReader(stream), true))
+                        {
+                            csvTable.Load(csvReader);
+                        }
+                        return View(csvTable);
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("File", "This file format is not supported");
+                        return View();
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("File", "Please Upload Your file");
+                }
+            }
+            return View();
         }
     }
 }
